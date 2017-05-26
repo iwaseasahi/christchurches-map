2c083a434cc4a3c10226d1ba4e7b2e50f0dba964
IwaseAsahi <eb50113@gmail.com>
2017-05-27 00:58:05 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/contact' => 'homes#contact'
+
   resources :maps, only: :index do
     collection do
       get '/search' => 'maps#search'
     end
   end
+
+  resources :churches, except: %i(index)
 end
