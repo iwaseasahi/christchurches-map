249273905d2d98babcde54a328293b8dbda51ae3
IwaseAsahi <eb50113@gmail.com>
2017-05-27 11:53:06 +0900

    教会個別ページの作成

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -1,7 +1,9 @@
 class ChurchesController < ApplicationController
   before_action :set_church, only: %i(show edit update destroy)
 
-  def show; end
+  def show
+    build_marker(@church)
+  end
 
   def new
     @church = Church.new
@@ -39,5 +41,19 @@ class ChurchesController < ApplicationController
 
   def church_params
     params.require(:church).permit(:name, :group_id, :postal_code, :prefecture_id, :address, :tel, :fax, :email, :url, :worshiping_time)
+  end
+
+  def build_marker(church)
+    @hash = Gmaps4rails.build_markers(church) do |church, marker|
+      marker.lat(church.latitude)
+      marker.lng(church.longitude)
+      marker.infowindow church_info(church)
+    end
+  end
+
+  def church_info(church)
+    info = ''
+    info += "<h4>#{church.address}</h4>"
+    info.html_safe
   end
 end
