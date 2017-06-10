86f6f8bc7beaec4266963ebd8923510dd6c03ede
IwaseAsahi <eb50113@gmail.com>
2017-06-10 13:20:17 +0900

    deviseのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/contact' => 'homes#contact'
