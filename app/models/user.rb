b5499dfc072a091cbe943b03cbf3fa6357a41dbe
IwaseAsahi <eb50113@gmail.com>
2017-06-10 13:18:04 +0900

    新規登録時にメール認証を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   ***REMOVED*** Include default devise modules. Others available are:
   ***REMOVED*** :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
