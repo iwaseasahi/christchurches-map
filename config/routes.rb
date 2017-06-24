113cdd10afe2916df4b702ac94aa42b60c9aff4b
IwaseAsahi <eb50113@gmail.com>
2017-06-25 00:51:29 +0900

    コメントの編集と更新

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,9 @@ Rails.application.routes.draw do
   post 'like/:church_id' => 'likes#like', as: 'like'
   delete 'unlike/:church_id' => 'likes#unlike', as: 'unlike'
 
-  post 'comment/:church_id' => 'comments#create', as: 'comment_create'
-  delete 'comment/:comment_id' => 'comments#destroy', as: 'comment_destroy'
+  resources :comments, only: %i(edit update destroy) do
+    collection do
+      post '/:church_id' => 'comments#create', as: 'create'
+    end
+  end
 end
