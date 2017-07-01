349ec9ccca0348335e4084e6134c6f5595f1e027
IwaseAsahi <eb50113@gmail.com>
2017-07-01 13:01:02 +0900

    アカウント確認の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
+
+  resources :users, only: %i(show)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/contact' => 'homes#contact'
