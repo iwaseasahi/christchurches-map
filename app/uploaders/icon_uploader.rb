3b51a43165d1820c8ea54071c453010b901a740f
IwaseAsahi <eb50113@gmail.com>
2017-07-01 19:33:38 +0900

    画像をアップロード時にリサイズ

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -37,9 +37,28 @@ class IconUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process :crop
+    process resize_to_fit: [100, 100]
+  end
+
+ def crop
+  manipulate! do |img|
+    # 中央から切り取り
+    gravity = Magick::CenterGravity
+    crop_w = img.columns
+    crop_h = img.rows
+    # 画像のサイズが縦横違った場合は小さい方に合わせてトリミング
+    if img.rows <= img.columns
+      crop_w = img.rows
+    else
+      crop_h = img.columns
+    end
+    img.crop!(gravity, crop_w, crop_h)
+    img = yield(img) if block_given?
+    img
+  end
+ end
 
   # アップロード可能な形式
   def extension_whitelist
