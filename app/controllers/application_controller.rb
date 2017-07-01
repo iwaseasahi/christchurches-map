d338d90abe0814488818746832f9b95d37f64429
IwaseAsahi <eb50113@gmail.com>
2017-07-01 18:46:48 +0900

    アイコン画像の変更機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i(last_name first_name))
-    devise_parameter_sanitizer.permit(:account_update, keys: %i(last_name first_name icon))
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
   }
 
-  resources :users, only: %i(show)
+  resources :users, only: %i(show) do
+    member do
+      put :upload_icon
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
