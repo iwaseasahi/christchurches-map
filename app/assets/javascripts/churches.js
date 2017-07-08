d6e8dbada1af311444eaffc2178221ddc0bb9af7
IwaseAsahi <eb50113@gmail.com>
2017-07-08 13:47:51 +0900

    画像アップロード時にローディング表示

--- app/assets/javascripts/churches.js
+++ app/assets/javascripts/churches.js
@@ -4,4 +4,10 @@ $(function() {
     var position = $(id).offset().top;
     $('html, body').animate({'scrollTop': position}, 'slow');
   });
+  $('#church_top_image').on('change', function (e) {
+    if (e.target.files[0].size > 0) {
+      $(this).parents('form').submit();
+      $('.loading').fadeIn();
+    }
+  });
 });
