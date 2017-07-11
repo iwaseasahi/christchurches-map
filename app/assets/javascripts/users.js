c6c3ee26c42be1d6c7a28386a2aba372897706c0
IwaseAsahi <eb50113@gmail.com>
2017-07-11 23:24:52 +0900

    アイコン変更時にローディング画面の表示

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,6 +2,7 @@ $(function() {
   $('#user_icon').on('change', function (e) {
     if (e.target.files[0].size > 0) {
       $(this).parents('form').submit();
+      $('.loading').fadeIn();
     }
   });
 });
