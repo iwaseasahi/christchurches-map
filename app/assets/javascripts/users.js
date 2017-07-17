51fe7356f6b2ffb7eab2220baba95415ef6ab382
IwaseAsahi <eb50113@gmail.com>
2017-07-17 15:11:08 +0900

    turbolinksの機能を一部解除

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -33,7 +33,7 @@ function validationUserForm() {
   });
 }
 
-$(function() {
+$(document).on('turbolinks:load', function() {
   $('#user_icon').on('change', function (e) {
     if (e.target.files[0].size > 0) {
       $(this).parents('form').submit();
