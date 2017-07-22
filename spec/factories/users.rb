333ce5baefb7f2be980da291071c632df8337f3f
IwaseAsahi <eb50113@gmail.com>
2017-07-22 19:14:56 +0900

    user.rbのテストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,17 @@
 
 FactoryGirl.define do
   factory :user do
+    ***REMOVED*** Fakerによる自動生成
+    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name }
+    email { Faker::Internet.safe_email('test') }
+    password { Faker::Internet.password(min_length = 6, max_length = 12) }
+  end
+
+  factory :user_test do
+    last_name '岩瀬'
+    first_name '旭'
+    password 'asahitest'
+    sequence(:email) { |n| "eb50113***REMOVED***{n}@yahoo.co.jp" }
   end
 end
