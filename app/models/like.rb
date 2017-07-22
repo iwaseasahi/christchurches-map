40eb05952e6f665cd3b897906f295d10fd7d4bcc
IwaseAsahi <eb50113@gmail.com>
2017-07-22 22:53:34 +0900

    リファクタリング

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,6 @@
 class Like < ApplicationRecord
-  validates :user_id,  presence: true
-  validates :church_id,  presence: true
+  validates :user_id,   presence: true
+  validates :church_id, presence: true
 
   belongs_to :user
   belongs_to :church
