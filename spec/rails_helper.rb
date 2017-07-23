13b5b23970d1adf2f16178df522d01cf46066813
IwaseAsahi <eb50113@gmail.com>
2017-07-23 22:34:43 +0900

    homes_controllerのテストコード

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,4 +50,14 @@ RSpec.configure do |config|
 
   # ファクトリを簡単に呼び出せるよう、Factory Girlの構文をインクルードする
   # config.include FactoryGirl::Syntax::Methods
+
+  # RSpecでコントローラーのテストを実行
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :view
+
+  %i[controller view request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: :type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: :type
+    config.include ::Rails::Controller::Testing::Integration, type: :type
+  end
 end
