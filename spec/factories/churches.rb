bdf9517df021eeb54b3cb0bd6f5cf8aa95f3514d
IwaseAsahi <eb50113@gmail.com>
2017-07-24 22:30:47 +0900

    コントローラーのテストコード作成

--- spec/factories/churches.rb
+++ spec/factories/churches.rb
@@ -2,5 +2,16 @@
 
 FactoryGirl.define do
   factory :church do
+    name 'サランパン'
+    group_id 176
+    prefecture_id 13
+    address '東京都新宿区百人町2-18-17'
+  end
+
+  factory :invalid_church do
+    name 'サランパン'
+    group_id 176
+    prefecture_id 13
+    address '東京都新宿区百人町2-18-17'
   end
 end
