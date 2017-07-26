f872df24d044dab6412a10ef4fbb00c6893894dd
IwaseAsahi <eb50113@gmail.com>
2017-07-26 22:55:18 +0900

    users_controller.rbのテストコード作成

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
 
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe 'GET #show' do
+    it '@userに要求されたユーザーに割り当てること' do
+      user = FactoryGirl.create(:user_test)
+      get :show, params: { id: user }
+      expect(assigns(:user)).to eq user
+    end
+
+    it ':showテンプレートを表示すること' do
+      user = FactoryGirl.create(:user_test)
+      get :show, params: { id: user }
+      expect(response).to render_template(:show)
     end
   end
 
