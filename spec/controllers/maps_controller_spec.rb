594935fe8619e38894603a94cd0a1122ef23dc2b
IwaseAsahi <eb50113@gmail.com>
2017-07-28 21:43:06 +0900

    maps_controller.rbのテストコード

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe MapsController, :type => :controller do
 
+  describe 'GET #index' do
+    context '有効な属性の場合' do
+      it ':indexテンプレートを表示' do
+        get :index
+        expect(response).to render_template(:index)
+      end
+    end
+  end
+
+  describe 'GET #search' do
+    context '有効な属性の場合' do
+      it ':searchテンプレートを表示' do
+        get :search
+        expect(response).to render_template(:search)
+      end
+
+      it '@churchesに要求された教会が入っていること' do
+        church = FactoryGirl.create(:church)
+        get :search, params: { q: { name_cont: church.name } }
+        expect(assigns(:churches).first).to eq(church)
+      end
+    end
+  end
+
 end
