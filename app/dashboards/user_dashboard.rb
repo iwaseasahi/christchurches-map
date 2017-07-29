f1dd2a99a9b15e1a4e690c01c2faf67ac3d89eae
IwaseAsahi <eb50113@gmail.com>
2017-07-29 10:13:18 +0900

    user_dashboardの編集

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -44,80 +44,62 @@ class UserDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :id,
+    :email,
     :likes,
-    :like_churches,
+    # :like_churches,
     :comments,
-    :comment_churches,
+    # :comment_churches,
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
   SHOW_PAGE_ATTRIBUTES = [
-    :likes,
-    :like_churches,
-    :comments,
-    :comment_churches,
     :id,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :confirmation_token,
-    :confirmed_at,
-    :confirmation_sent_at,
-    :unconfirmed_email,
     :last_name,
     :first_name,
+    :encrypted_password,
     :church_id,
     :gender,
     :icon,
     :is_test,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :likes,
+    # :like_churches,
+    :comments,
+    # :comment_churches,
     :soft_destroyed_at,
-    :created_at,
-    :updated_at,
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
   FORM_ATTRIBUTES = [
-    :likes,
-    :like_churches,
-    :comments,
-    :comment_churches,
     :email,
-    :encrypted_password,
-    :reset_password_token,
-    :reset_password_sent_at,
-    :remember_created_at,
-    :sign_in_count,
-    :current_sign_in_at,
-    :last_sign_in_at,
-    :current_sign_in_ip,
-    :last_sign_in_ip,
-    :confirmation_token,
-    :confirmed_at,
-    :confirmation_sent_at,
-    :unconfirmed_email,
     :last_name,
     :first_name,
+    :encrypted_password,
     :church_id,
     :gender,
     :icon,
     :is_test,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :likes,
+    :like_churches,
+    :comments,
+    :comment_churches,
     :soft_destroyed_at,
   ].freeze
 
   # Overwrite this method to customize how users are displayed
   # across all pages of the admin dashboard.
   #
-  # def display_resource(user)
-  #   "User ##{user.id}"
-  # end
+  def display_resource(user)
+    "ユーザーID: #{user.id}"
+  end
 end
