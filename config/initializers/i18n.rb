61dd543db65b9acced3cc1565f99a3f38062f0f4
IwaseAsahi <eb50113@gmail.com>
2017-07-29 09:23:56 +0900

    i18nをフォルダ管理

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -1,1 +1,6 @@
+# デフォルトのlocaleを日本語(:ja)
 Rails.application.config.i18n.default_locale = :ja
+
+# 翻訳ファイルのディレクトを追加
+Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
