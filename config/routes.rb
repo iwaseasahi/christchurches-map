7d3c086fffaed3c36764a5718676b2fb51af487d
岩瀬旭 <eb50113@yahoo.co.jp>
2017-08-18 23:33:26 +0900

    教会写真のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :users, only: %i(show) do
     member do
-      put :upload_icon
+      put :upload_icog
     end
   end
 
@@ -41,6 +41,7 @@ Rails.application.routes.draw do
     member do
       put :upload_top_image
     end
+    resources :photos, only: %i[index create destroy]
   end
 
   post 'like/:church_id' => 'likes#like', as: 'like'
