3ec8dd47bb610e4cc90030549b1fda31107d0be1
岩瀬旭 <eb50113@yahoo.co.jp>
2017-08-25 20:58:24 +0900

    usersテーブルから論理削除カラムを削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -24,13 +24,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   ***REMOVED*** end
 
   ***REMOVED*** DELETE /resource
-  def destroy
-    resource.soft_destroy
-    Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name)
-    set_flash_message! :notice, :destroyed
-    yield resource if block_given?
-    respond_with_navigational(resource){ redirect_to after_sign_out_path_for(resource_name) }
-  end
+  ***REMOVED*** def destroy
+  ***REMOVED***   super
+  ***REMOVED*** end
 
   ***REMOVED*** GET /resource/cancel
   ***REMOVED*** Forces the session data which is usually expired after sign
