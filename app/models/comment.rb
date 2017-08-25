25b5ca706a78624679d2ec32a3751aaf22694cc6
岩瀬旭 <eb50113@yahoo.co.jp>
2017-08-25 21:19:05 +0900

    コメントを物理削除に変更

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ApplicationRecord
-  soft_deletable dependent_associations: %i(user church)
   validates :comment,   presence: true
   validates :user_id,   presence: true
   validates :church_id, presence: true
