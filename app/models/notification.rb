4b376b0f11db55f824345109aae8cf978ea554b6
岩瀬旭 <eb50113@yahoo.co.jp>
2017-08-26 17:01:40 +0900

    ダブルをシングルクォーテーションに変更

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification
   def self.notify_error_email(e)
     Rails.logger.error e
-    Rails.logger.error e.backtrace.join("\n")
+    Rails.logger.error e.backtrace.join('\n')
     ExceptionNotifier.notify_exception(e)
   end
 end
