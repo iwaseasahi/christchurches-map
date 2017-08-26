a65657c52e5456ef5c728b7eca9fa19f51c6127e
岩瀬旭 <eb50113@yahoo.co.jp>
2017-08-26 16:35:59 +0900

    アップローダーをリファクタリング

--- app/uploaders/top_image_uploader.rb
+++ app/uploaders/top_image_uploader.rb
@@ -19,7 +19,7 @@ class TopImageUploader < CarrierWave::Uploader::Base
 
   # 画像がアップロードされていない場合の対応
   # public/images/default_top_image.jpgを読み込む
-  def default_url(*args)
+  def default_url(*_args)
     # For Rails 3.1+ asset pipeline compatibility:
     # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
     # "/images/fallback/" + [version_name, "default.png"].compact.join('_')
@@ -40,39 +40,38 @@ class TopImageUploader < CarrierWave::Uploader::Base
   end
 
   def crop
-   manipulate! do |img|
-     # 中央から切り取り
-     gravity = Magick::CenterGravity
-     crop_w = img.columns
-     crop_h = img.rows
-     img.crop!(gravity, crop_w, crop_h)
-     img = yield(img) if block_given?
-     img
-   end
+    manipulate! do |img|
+      # 中央から切り取り
+      gravity = Magick::CenterGravity
+      crop_w = img.columns
+      crop_h = img.rows
+      img.crop!(gravity, crop_w, crop_h)
+      img = yield(img) if block_given?
+      img
+    end
   end
 
   # 元画像の削除
-  def remove_original_file(original_file)
+  def remove_original_file(_original_file)
     return if version_name.present?
     file.delete if file.exists?
   end
 
   # アップロード可能な形式
   def extension_whitelist
-    %w(jpg jpeg gif png)
+    %w[jpg jpeg gif png]
   end
 
   # アップロード時のファイル名を指定
   def filename
-    if original_filename.present?
-      "#{model.id}_#{secure_token}.#{file.extension}"
-    end
+    return if original_filename.blank?
+    "#{model.id}_#{secure_token}.#{file.extension}"
   end
 
   protected
 
   def secure_token
     var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
   end
 end
