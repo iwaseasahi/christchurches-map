d6eb3769af7aa2d08d514348173dfe2bd6243a7e
岩瀬旭 <eb50113@yahoo.co.jp>
2018-01-08 22:20:06 +0900

    ログインしていないと、教会の詳細ページにリダイレクトする

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,6 @@
 class PhotosController < ApplicationController
   before_action :set_church
+  before_action :check_user_login
   before_action :set_photo, only: :destroy
 
   def index
@@ -25,6 +26,10 @@ class PhotosController < ApplicationController
 
   def set_church
     @church = Church.find(params[:church_id])
+  end
+
+  def check_user_login
+    redirect_to church_path(@church) unless user_signed_in?
   end
 
   def set_photo
