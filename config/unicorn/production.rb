eb5aec448cf4e2d1ac34eea69630630f320e76df
岩瀬旭 <eb50113@yahoo.co.jp>
2018-03-24 16:30:04 +0900

    シングルクォーテーションに変更する

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,7 +1,7 @@
 # set lets
 $worker  = 2
 $timeout = 30
-$app_dir = "/var/www/projects/christchurches-map/current"
+$app_dir = '/var/www/projects/christchurches-map/current'
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
