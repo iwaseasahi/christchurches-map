0725df89b77cef4d1f4207536e2d7818f999151d
岩瀬旭 <eb50113@yahoo.co.jp>
2018-03-24 16:12:22 +0900

    シングルクォーテーションに変更する

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,15 @@
 # config valid only for current version of Capistrano
-lock "3.8.1"
+lock '3.8.1'
 
-set :application, "christchurches-map"
-set :repo_url, "git@github.com:iwaseasahi/christchurches-map.git"
+set :application, 'christchurches-map'
+set :repo_url, 'git@github.com:iwaseasahi/christchurches-map.git'
 
 # Default branch is :master
 set :branch, 'master'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/var/www/projects/christchurches-map"
+set :deploy_to, '/var/www/projects/christchurches-map'
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
