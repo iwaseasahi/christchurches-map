2f53155824d8166fcd0984199f39359ab5c546c9
岩瀬旭 <eb50113@yahoo.co.jp>
2019-05-19 19:53:22 +0900

    壊れていたテストコードを修正する #258

--- spec/controllers/maps_controller_spec.rb
+++ spec/controllers/maps_controller_spec.rb
@@ -13,14 +13,20 @@ RSpec.describe MapsController, :type => :controller do
 
   describe 'GET #search' do
     context '有効な属性の場合' do
+      let!(:group) { create(:group) }
+      let!(:church) { create(:church, group: group) }
+
+      subject { get :search, params: { q: { name_cont: church.name } } }
+
       it ':searchテンプレートを表示' do
-        get :search
+        subject
+
         expect(response).to render_template(:search)
       end
 
       it '@churchesに要求された教会が入っていること' do
-        church = FactoryGirl.create(:church)
-        get :search, params: { q: { name_cont: church.name } }
+        subject
+
         expect(assigns(:churches).first).to eq(church)
       end
     end
