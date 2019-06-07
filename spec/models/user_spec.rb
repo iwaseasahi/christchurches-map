3b06f052b2fc551755e9b1c7b918369c6a3d6a31
岩瀬旭 <eb50113@yahoo.co.jp>
2019-06-08 08:55:40 +0900

    FactoryBot を省略する #274

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,33 +3,33 @@ require 'rails_helper'
 RSpec.describe User, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it '有効なファクトリを持つこと' do
-    expect(FactoryBot.build(:user)).to be_valid
+    expect(build(:user)).to be_valid
   end
 
   it '有効なファクトリで登録できること' do
-    expect(FactoryBot.create(:user)).to be_valid
+    expect(create(:user)).to be_valid
   end
 
   it '姓が未入力であれば無効な状態であること' do
-    user = FactoryBot.build(:user, last_name: nil)
+    user = build(:user, last_name: nil)
     user.valid?
     expect(user.errors[:last_name]).to include('を入力してください')
   end
 
   it '名が未入力であれば無効な状態であること' do
-    user = FactoryBot.build(:user, first_name: nil)
+    user = build(:user, first_name: nil)
     user.valid?
     expect(user.errors[:first_name]).to include('を入力してください')
   end
 
   it 'メールアドレスが未入力であれば無効な状態であること' do
-    user = FactoryBot.build(:user, email: nil)
+    user = build(:user, email: nil)
     user.valid?
     expect(user.errors[:email]).to include('を入力してください')
   end
 
   it 'パスワードが未入力であれば無効な状態であること' do
-    user = FactoryBot.build(:user, password: nil)
+    user = build(:user, password: nil)
     user.valid?
     expect(user.errors[:password]).to include('を入力してください')
   end
@@ -52,7 +52,7 @@ RSpec.describe User, :type => :model do
   end
 
   it 'フルネームを表示すること' do
-    user = FactoryBot.build(:user)
+    user = build(:user)
     expect(user.full_name).to eq("#{user.last_name} #{user.first_name}")
   end
 end
