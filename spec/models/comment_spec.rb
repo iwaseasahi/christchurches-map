4474d4909bfb1139629541210e42e48fbd00712b
岩瀬旭 <eb50113@yahoo.co.jp>
2019-06-08 08:26:06 +0900

    FactoryGirl を FactoryBot に置換する #274

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,29 +3,29 @@ require 'rails_helper'
 RSpec.describe Comment, :type => :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   it '有効なファクトリを持つこと' do
-    expect(FactoryGirl.build(:comment)).to be_valid
+    expect(FactoryBot.build(:comment)).to be_valid
   end
 
   it 'コメントが未入力であれば、無効な状態であること' do
-    comment = FactoryGirl.build(:comment, comment: nil)
+    comment = FactoryBot.build(:comment, comment: nil)
     comment.valid?
     expect(comment.errors[:comment]).to include('を入力してください')
   end
 
   it 'ユーザーIDが未入力であれば、無効な状態であること' do
-    comment = FactoryGirl.build(:comment, user_id: nil)
+    comment = FactoryBot.build(:comment, user_id: nil)
     comment.valid?
     expect(comment.errors[:user_id]).to include('を入力してください')
   end
 
   it '教会IDが未入力であれば、無効な状態であること' do
-    comment = FactoryGirl.build(:comment, church_id: nil)
+    comment = FactoryBot.build(:comment, church_id: nil)
     comment.valid?
     expect(comment.errors[:church_id]).to include('を入力してください')
   end
 
   it 'timeメソッドがnilにならないこと' do
-    comment = FactoryGirl.build(:comment)
+    comment = FactoryBot.build(:comment)
     expect(comment.time).not_to eq nil
   end
 end
