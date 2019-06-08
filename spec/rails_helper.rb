03e3f7d70f2d3fd1f8164f160d7ec8e38525e530
岩瀬旭 <eb50113@yahoo.co.jp>
2019-06-08 09:01:11 +0900

    factory_bot の設定を移動する #274

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'support/factory_bot'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -47,9 +48,6 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-
-  # ファクトリを簡単に呼び出せるよう、Factory Girlの構文をインクルードする
-  # config.include FactoryBot::Syntax::Methods
 
   # RSpecでコントローラーのテストを実行
   config.include Devise::Test::ControllerHelpers, type: :controller
