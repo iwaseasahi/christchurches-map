1f18491c2a343adf7c670198c6c5c4fe2c7e5eb7
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-01 07:02:18 +0900

    Add spec prefecture #309

---
+++ spec/models/prefecture_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Prefecture, type: :model do
+  describe '#set_position' do
+    specify do
+      prefecture = create(:prefecture)
+
+      expect(prefecture.set_position[:lat]).to eq prefecture.latitude
+      expect(prefecture.set_position[:lng]).to eq prefecture.longitude
+      expect(prefecture.set_position[:zoom]).to eq prefecture.zoom_level
+    end
+  end
+end
