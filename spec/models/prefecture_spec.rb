4071479332fdbd3674dc275b3d12caf49a062866
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-01 13:15:49 +0900

    Add trait tokyo #315

--- spec/models/prefecture_spec.rb
+++ spec/models/prefecture_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Prefecture, type: :model do
   describe '#set_position' do
     specify do
-      prefecture = create(:prefecture)
+      prefecture = create(:prefecture, :tokyo)
 
       expect(prefecture.set_position[:lat]).to eq prefecture.latitude
       expect(prefecture.set_position[:lng]).to eq prefecture.longitude
