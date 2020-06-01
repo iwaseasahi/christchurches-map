bb7c6b49f11f6e112cbed03ffcf7ec8d85373498
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-01 13:15:49 +0900

    Add trait tokyo #315

--- spec/factories/prefectures.rb
+++ spec/factories/prefectures.rb
@@ -2,9 +2,11 @@
 
 FactoryBot.define do
   factory :prefecture do
-    name { '東京都' }
-    latitude { 35.68944 }
-    longitude { 139.69167 }
-    zoom_level { 5 }
+    trait :tokyo do
+      name { '東京都' }
+      latitude { 35.68944 }
+      longitude { 139.69167 }
+      zoom_level { 5 }
+    end
   end
 end
