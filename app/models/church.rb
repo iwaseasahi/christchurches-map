a7a34d875c293aec3a334421e7449442762a2ee3
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-01 13:52:53 +0900

    Fix church #315

--- app/models/church.rb
+++ app/models/church.rb
@@ -2,18 +2,19 @@ class Church < ApplicationRecord
   soft_deletable
   has_paper_trail
 
-  validates :name,          presence: true
-  validates :group_id,      presence: true
-  validates :prefecture_id, presence: true
-  validates :address,       presence: true
-
-  mount_uploader :top_image, TopImageUploader
-
   belongs_to :group
   belongs_to :prefecture
+
   has_many   :likes, dependent: :destroy
   has_many   :comments, -> { order(created_at: :desc) }, dependent: :destroy
   has_many   :photos, dependent: :destroy
+
+  validates :name, presence: true
+  validates :group, presence: true
+  validates :prefecture, presence: true
+  validates :address, presence: true
+
+  mount_uploader :top_image, TopImageUploader
 
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
