e872ec1fab55c47ceaef8bb3b4619c35a1d5662f
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-01 13:56:37 +0900

    Fix comment #315

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :church
 
-  validates :comment,   presence: true
-  validates :user_id,   presence: true
-  validates :church_id, presence: true
+  validates :comment, presence: true
+  validates :user, presence: true
+  validates :church, presence: true
 
   def time
     created_at.strftime('%Y-%m-%d %H:%M')
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,28 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
   it '有効であること' do
-    user = create(:user)
-    church = create(:church)
+    comment = build(:comment)
 
-    expect(build(:comment, user: user, church: church)).to be_valid
+    expect(comment).to be_valid
   end
 
   it 'コメントが未入力であれば、無効な状態であること' do
     comment = build(:comment, comment: nil)
     comment.valid?
+
     expect(comment.errors[:comment]).to include('を入力してください')
   end
 
   it 'ユーザーIDが未入力であれば、無効な状態であること' do
     comment = build(:comment, user_id: nil)
     comment.valid?
-    expect(comment.errors[:user_id]).to include('を入力してください')
+
+    expect(comment.errors[:user]).to include('を入力してください')
   end
 
   it '教会IDが未入力であれば、無効な状態であること' do
     comment = build(:comment, church_id: nil)
     comment.valid?
-    expect(comment.errors[:church_id]).to include('を入力してください')
+
+    expect(comment.errors[:church]).to include('を入力してください')
   end
 
   context '#time' do
