ffd5e59ab629d49fa6325569d7437662240474b7
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 17:32:15 +0900

    Ref like_rankings #323

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get '/contact'       => 'homes#contact'
-  get '/likes_ranking' => 'homes#likes_ranking'
+
+  resources :like_rankings, only: :index
 
   resources :maps, only: :index do
     collection do
