b83e446115df680b6ec7b95a233b48a92c31b4ad
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 22:45:16 +0900

    Add spec maps #329

--- spec/factories/churches.rb
+++ spec/factories/churches.rb
@@ -13,5 +13,15 @@ FactoryBot.define do
       email { Faker::Internet.email }
       url { Faker::Internet.url }
     end
+
+    trait :shinjuku_shalom do
+      name { '新宿シャローム教会' }
+      address { '東京都新宿区百人町１丁目２３−２４ アミューズ大久保ビル 2F' }
+      postal_code { '169-0073' }
+      latitude { 35.6992842 }
+      longitude { 139.6952906 }
+      tel { '03-3371-7558' }
+      url { 'http://shinjuku-shalom.com/' }
+    end
   end
 end
