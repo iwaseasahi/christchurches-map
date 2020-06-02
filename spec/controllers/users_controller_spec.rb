0030bf06fa870c855d3d888207ce5426e40092e6
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 10:31:13 +0900

    Fix user spec #317

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,19 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
-
   describe 'GET #show' do
     it '@userに要求されたユーザーに割り当てること' do
-      user = create(:user_test)
+      user = create(:user)
       get :show, params: { id: user }
       expect(assigns(:user)).to eq user
     end
 
     it ':showテンプレートを表示すること' do
-      user = create(:user_test)
+      user = create(:user)
       get :show, params: { id: user }
       expect(response).to render_template(:show)
     end
   end
-
 end
