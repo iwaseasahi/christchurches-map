d375d664f895d814d7195b99685b60d12d14e58f
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 18:06:48 +0900

    Ref contacts #325

--- config/routes.rb
+++ config/routes.rb
@@ -25,8 +25,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get '/contact'       => 'homes#contact'
-
+  resources :contacts, only: :index
   resources :like_rankings, only: :index
 
   resources :maps, only: :index do
