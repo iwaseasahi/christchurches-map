e45e762059eaa1d3e8f95952600e896d47e071e5
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 10:09:07 +0900

    Auto correct rubocop-faker #317

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
   factory :user do
     last_name { Faker::Name.last_name }
     first_name { Faker::Name.first_name }
-    email { Faker::Internet.safe_email('test') }
-    password { Faker::Internet.password(min_length = 6, max_length = 12) }
+    email { Faker::Internet.safe_email(name: 'test') }
+    password { Faker::Internet.password(min_length: min_length = 6, max_length: max_length = 12) }
   end
 
   factory :user_test, class: :user do
