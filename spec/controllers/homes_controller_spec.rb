bbbd1d686780aeeeb330745d02a960cf96feda02
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-02 18:19:44 +0900

    Ref homes #327

--- spec/controllers/homes_controller_spec.rb
+++ spec/controllers/homes_controller_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe HomesController, :type => :controller do
+RSpec.describe HomesController, type: :controller do
   describe 'GET root_path' do
-    it 'topテンプレートを表示すること' do
+    it 'indexテンプレートを表示すること' do
       expect(get: root_url(subdomain: nil)).to route_to(
         controller: 'homes',
-        action: 'top')
+        action: 'index')
     end
   end
 end
