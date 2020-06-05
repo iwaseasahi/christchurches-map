900aedd5a7d46987729782032926288661a8cdf0
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-05 20:54:05 +0900

    Use authenticate_user #349

--- app/controllers/churches/upload_top_images_controller.rb
+++ app/controllers/churches/upload_top_images_controller.rb
@@ -1,6 +1,6 @@
 class Churches::UploadTopImagesController < ApplicationController
   before_action :set_church
-  before_action :check_user_login
+  before_action :authenticate_user!
 
   def create
     if @church.update(top_image_params)
@@ -14,10 +14,6 @@ class Churches::UploadTopImagesController < ApplicationController
 
   def set_church
     @church = Church.find(params[:church_id])
-  end
-
-  def check_user_login
-    redirect_to church_path(@church) unless user_signed_in?
   end
 
   def top_image_params
