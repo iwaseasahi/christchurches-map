26b369e2e9e8728942daa3f65bb43c637506ed1a
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-05 13:22:59 +0900

    Ref upload icons #338

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show upload_icon]
-
-  def show; end
-
-  def upload_icon
-    if @user.update(icon_params)
-      redirect_to user_path(@user.id)
-    else
-      redirect_to user_path(@user.id), alert: 'アイコンが変更できませんでした。'
-    end
-  end
-
-  private
-
-  def set_user
+  def show
     @user = User.find(params[:id])
-  end
-
-  def icon_params
-    params.require(:user).permit(:icon)
   end
 end
