6fcb0d9ef48789ef713d438bf70fc1a98c0197f4
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-05 16:59:40 +0900

    Delete unnecessary method #341
    crop を実行する前と後で同じだったので削除します。

--- app/uploaders/top_image_uploader.rb
+++ app/uploaders/top_image_uploader.rb
@@ -17,20 +17,7 @@ class TopImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :crop
     process resize_to_fit: [800, 400]
-  end
-
-  def crop
-    manipulate! do |img|
-      # 中央から切り取り
-      gravity = Magick::CenterGravity
-      crop_w = img.columns
-      crop_h = img.rows
-      img.crop!(gravity, crop_w, crop_h)
-      img = yield(img) if block_given?
-      img
-    end
   end
 
   # 元画像の削除
