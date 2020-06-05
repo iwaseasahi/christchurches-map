162fdb106b54c32fe4dcd3d0f49901ab9b01a323
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-05 16:22:45 +0900

    Add spec top image uploader #341

---
+++ spec/system/churches_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature '教会', type: :system do
+  scenario '閲覧できる' do
+    church = create(:church, :shinjuku_shalom)
+
+    visit church_path(church)
+
+    expect(page).to have_current_path church_path(church)
+    expect(page).to have_text church.name
+  end
+
+  scenario 'ログインしていれば、編集できる' do
+    church = create(:church, :shinjuku_shalom)
+    sign_in
+
+    visit church_path(church)
+    click_link '編集する'
+
+    expect(page).to have_current_path edit_church_path(church)
+
+    click_button '更新する'
+
+    expect(page).to have_current_path church_path(church)
+  end
+
+  scenario 'ログインしていなければ、編集できない', js: true do
+    church = create(:church, :shinjuku_shalom)
+
+    visit church_path(church)
+    click_link '編集する'
+
+    expect(page).not_to have_current_path edit_church_path(church)
+    expect(page).to have_text 'この機能を利用するにはユーザー登録が必要です。'
+  end
+
+  scenario 'トップ画像を変更できる', js: true do
+    church = create(:church, :shinjuku_shalom)
+    sign_in
+
+    visit church_path(church)
+    click_link '編集する'
+
+    page.attach_file('church_top_image', 'spec/support/church_top_image.jpg', make_visible: true)
+
+    expect { church.reload }.to change { church.top_image_identifier }
+  end
+end
