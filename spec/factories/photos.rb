d3e3c2bf38a8ad19657fa5a0503090543d2b93f4
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-07 20:35:58 +0900

    Ref images to assets #363

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :photo do
     association :church
 
-    file_name { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/images/logo_image.jpg'), 'image/jpeg') }
+    file_name { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/assets/logo_image.jpg'), 'image/jpeg') }
     sort_of_number { Random.rand(1...10) }
   end
 end
