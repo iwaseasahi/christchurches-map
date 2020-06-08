5f8445bc15319e9c1eab4003083270460479cba7
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-08 15:49:35 +0900

    Ref likes #365

--- config/routes/main.rb
+++ config/routes/main.rb
@@ -29,12 +29,10 @@ end
 resources :churches, except: %i[index] do
   scope module: :churches do
     resource :upload_top_image, only: :create
+    resource :likes, only: %i[create destroy]
   end
   resources :photos, only: %i[index create destroy]
 end
-
-post 'like/:church_id'     => 'likes#like', as: 'like'
-delete 'unlike/:church_id' => 'likes#unlike', as: 'unlike'
 
 resources :comments, only: %i[edit update destroy] do
   collection do
