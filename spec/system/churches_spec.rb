7a266c2d382ab2e0391de94b4de9a87484676e13
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-09 08:19:24 +0900

    Ref spec #389

--- spec/system/churches_spec.rb
+++ spec/system/churches_spec.rb
@@ -33,32 +33,4 @@ feature '教会', type: :system do
     expect(page).not_to have_current_path edit_church_path(church)
     expect(page).to have_text 'この機能を利用するにはユーザー登録が必要です。'
   end
-
-  scenario 'トップ画像を変更できる', js: true do
-    church = create(:church, :shinjuku_shalom)
-    sign_in
-
-    visit church_path(church)
-    click_link '編集する'
-
-    page.attach_file('church_top_image', 'spec/support/assets/church_top_image.jpg', make_visible: true)
-
-    expect { church.reload }.to change { church.top_image_identifier }
-
-    click_button '更新する'
-
-    expect(page.find('.church-header')['style']).to include church.top_image_identifier
-  end
-
-  scenario '許可していない拡張子はアップロードできない', js: true do
-    church = create(:church, :shinjuku_shalom)
-    sign_in
-
-    visit church_path(church)
-    click_link '編集する'
-
-    page.attach_file('church_top_image', 'spec/support/assets/upload_not_allowed.csv', make_visible: true)
-
-    expect(page).to have_text 'トップ画像が変更できませんでした。'
-  end
 end
