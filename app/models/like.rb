3dee0993e4c36d5ed1aa182d882e371068edeb3e
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-08 15:38:43 +0900

    Add uniq index #365

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,6 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :church
 
-  validates :user, presence: true
+  validates :user, presence: true, uniqueness: { scope: :church_id, case_sensitive: false }
   validates :church, presence: true
 end
