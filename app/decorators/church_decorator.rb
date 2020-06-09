7603bb0034872c8f6d749d20029d71632a243ff6
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-09 12:45:40 +0900

    Ref infowindow #392

--- app/decorators/church_decorator.rb
+++ app/decorators/church_decorator.rb
@@ -16,4 +16,13 @@ module ChurchDecorator
   def info_only_address
     "<h4>#{address}</h4>".html_safe
   end
+
+  def infowindow
+    info = "<h4>#{name}</h4>"
+    info += "<p>#{group.name}</p>"
+    info += "<p>#{address}</p>"
+    info += "<p><a href='/churches/#{id}'>詳細はこちら</a></p>"
+    info += "<p><a href=#{url} target='_blank'>ホームページはこちら</a></p>" if url.present?
+    info.html_safe
+  end
 end
