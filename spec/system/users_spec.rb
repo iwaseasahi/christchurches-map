d4ecd44a1c10f8e35e3e5d055ee9a2f64c0e5712
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-09 12:22:32 +0900

    Ref spec #370

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 feature 'ユーザー', type: :system do
   scenario '閲覧できる' do
     sign_in
+    decorated_user = ActiveDecorator::Decorator.instance.decorate(@user)
 
     within(:css, '#mainNav') do
-      click_link @user.full_name
+      click_link decorated_user.full_name
     end
 
     expect(page).to have_current_path user_path(@user)
@@ -13,9 +14,10 @@ feature 'ユーザー', type: :system do
 
   scenario 'アイコンを変更できる', js: true do
     sign_in
+    decorated_user = ActiveDecorator::Decorator.instance.decorate(@user)
 
     within(:css, '#mainNav') do
-      click_link @user.full_name
+      click_link decorated_user.full_name
     end
 
     page.attach_file('user_icon', 'spec/support/assets/profile_image.png', make_visible: true)
