3652e27a7d8c08478a23ec728c150d90e02e013f
岩瀬旭 <eb50113@yahoo.co.jp>
2020-06-11 10:44:22 +0900

    Fix order #405

--- app/controllers/like_rankings_controller.rb
+++ app/controllers/like_rankings_controller.rb
@@ -1,6 +1,5 @@
 class LikeRankingsController < ApplicationController
   def index
-    church_ids = Like.group(:church_id).order('count_church_id desc').limit(10).count(:church_id).keys
-    @churches  = Church.where(id: church_ids)
+    @churches = Church.joins(:likes).group(:church_id).order('count(church_id) desc').limit(10)
   end
 end
