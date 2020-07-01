82c7b49776222ff5e786f2d482c09f3b4a5de1f4
岩瀬旭 <eb50113@yahoo.co.jp>
2020-07-02 08:12:14 +0900

    Remove limit #518

--- app/controllers/like_rankings_controller.rb
+++ app/controllers/like_rankings_controller.rb
@@ -1,5 +1,5 @@
 class LikeRankingsController < ApplicationController
   def index
-    @churches = Church.joins(:likes).group(:church_id).order('count(church_id) desc').limit(10)
+    @churches = Church.joins(:likes).group(:church_id).order('count(church_id) desc')
   end
 end
